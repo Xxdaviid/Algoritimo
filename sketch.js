7f5d5c2e01d74675121dc0f356a58710b14e1243
Xxdaviid <93736528+Xxdaviid@users.noreply.github.com>
2022-01-24 16:36:35 -0300

    24/01/2022

--- sketch.js
+++ sketch.js
@@ -1,32 +1,72 @@
-var mousevivo, mouse;
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
-function setup() {
-  createCanvas(800,400);
-  mouse = createSprite(200, 200, 50, 80);
-  mouse.shapeColor = "green";
-  mouse.debug = true;
-  mousevivo = createSprite(400,800,80,30);
-  mousevivo.shapeColor = "green";
-  mousevivo.debug = true;
-  console.log(mouse.width/2);
-  console.log(mousevivo.width/2);
+var engine, world;
+var box1, box2, ground, box3, box4, box5;
+var pig1, pig2;
+var log1, log2, log3, log4;
+var bird;
+var imagemfundo;
+var plataforma;
+var estilingue;
+
+function preload(){
+imagemfundo= loadImage("sprites/bg.png");
 }
 
-function draw() {
-  background(255,255,255); 
-  mousevivo.x = World.mouseX;
-  mousevivo.y = World.mouseY; 
+function setup(){
+var canvas = createCanvas(1200,400);
+engine = Engine.create();
+world = engine.world;
 
-  if( mousevivo.x - mouse.x < mouse.width/2 + mousevivo.width/2                                                                                   
-    &&mouse.x - mousevivo.x < mouse.width/2 + mousevivo.width/2  
-    &&mousevivo.y - mouse.y < mouse.height/2 + mousevivo.height/2                                                                                   
-    &&mouse.y - mousevivo.y < mouse.height/2 + mousevivo.height/2  ){
-    mouse.shapeColor = "red";
-    mousevivo.shapeColor = "red";
-  } else {
-    mouse.shapeColor = "green";
-    mousevivo.shapeColor = "green";
-  }
+box1 = new Box(700,320,70,70);
+box2 = new Box(920,320,70,70);
+ground = new Ground(600,400,1200,20);
+pig1 = new Pig(810,350); 
+log1= new Log(810,260,300,PI/2);
 
-  drawSprites();
+box3= new Box(700,240,70,70);
+box4= new Box(920,240,70,70);
+pig2= new Pig(810,220);
+log2= new Log(810,180,300,PI/2);
+
+box5= new Box(810,160,70,70);
+log3= new Log(760,120,150,PI/7);
+log4= new Log(870,120,150,-PI/7);
+
+bird = new Bird(200,50);
+
+plataforma = new Ground(150,305,300,170);
+
+
+estilingue= new Estilingue(bird.body,{x:200,y:50});
+}
+
+function draw(){
+background(imagemfundo);
+Engine.update(engine);
+box1.display();
+box2.display();
+ground.display();
+pig1.display();
+log1.display();
+box3.display();
+box4.display();
+pig2.display();
+log2.display();
+box5.display();
+log3.display();
+log4.display();
+bird.display();
+plataforma.display();
+estilingue.display();
+}
+
+function mouseDragged(){
+Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});    
+}
+function mouseReleased(){
+estilingue.fly();
 }
